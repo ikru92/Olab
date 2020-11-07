d950d599ec98b6d43f5c478b2d774d20c7a02e27
Akhil Krishnakumar <akhilkrishnakumar@yahoo.com>
2020-11-08 00:39:40 +0100

    http service and listing all the apps

--- src/app/components/component/child/new/new.component.ts
+++ src/app/components/component/child/new/new.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
-
+import { HttpService } from '../../../../services/http.service'
 
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.css']
 })
-export class NewComponent implements OnInit {
-  model: NgbDateStruct;
-  constructor() { }
 
+export class NewComponent implements OnInit {
+  show = false;
+  cmpt = {
+    name: "",
+    quantityLeft: 0,
+    dealer: "",
+  }
+  constructor(public httpsvc: HttpService) { }
   ngOnInit(): void {
   }
-
+  saveComponent(cmpt): void {
+    this.httpsvc.create("component", cmpt)
+      .subscribe(
+        response => {
+          this.show = true;
+          this.cmpt = {
+            name: "",
+            quantityLeft: 0,
+            dealer: "",
+          }
+          setTimeout(() => {
+            this.show = false;
+          }, 3000);
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }
